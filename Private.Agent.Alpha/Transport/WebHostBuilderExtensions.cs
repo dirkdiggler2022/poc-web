6e1272e5eb1eeddd7df0332da885a50426d1bc90
Chris McGinn <Chris.McGinn@delinea.com>
2024-05-30 04:26:07 -0400

    added 3 agents

---
+++ Private.Agent.Alpha/Transport/WebHostBuilderExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Connections;
+
+namespace Private.Agent.Alpha.Transport;
+
+public static class WebHostBuilderExtensions
+{
+    public static IWebHostBuilder UseTunnelTransport(this IWebHostBuilder hostBuilder, string url, Action<TunnelOptions>? configure = null)
+    {
+        ArgumentNullException.ThrowIfNull(url);
+
+        hostBuilder.ConfigureKestrel(options =>
+        {
+            options.Listen(new UriEndPoint(new Uri(url)));
+        });
+
+        return hostBuilder.ConfigureServices(services =>
+        {
+            services.AddSingleton<IConnectionListenerFactory, TunnelConnectionListenerFactory>();
+
+            if (configure is not null)
+            {
+                services.Configure(configure);
+            }
+        });
+    }
+}
---
+++ Private.Agent.Beta/Program.cs
@@ -0,0 +1,31 @@
+namespace Private.Agent.Beta
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddReverseProxy()
+                .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+            var url = builder.Configuration["Tunnel:Url"]!;
+
+            //overload command line arg for url
+            if (args.Length > 0 && Uri.TryCreate(args[0].ToString(), UriKind.Absolute, out var test))
+                url = test.ToString();
+
+
+            builder.WebHost.UseTunnelTransport(url, options =>
+            {
+                options.Transport = url.Contains("connect-h2") ? TransportType.HTTP2 : TransportType.WebSockets;
+            });
+
+            var app = builder.Build();
+
+            app.MapReverseProxy();
+
+            app.Run();
+        }
+    }
+}
